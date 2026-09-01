c1e74c5eaa9ff689d351fcd197819a2d7ff1bfd3
JazHyc <feronest@yahoo.com>
2026-09-01 21:34:02 +0200

    Reduce frontier title spacing

--- scripts/plot_tool_trajectory_ood_frontier.py
+++ scripts/plot_tool_trajectory_ood_frontier.py
@@ -413,7 +413,7 @@ def plot_frontier(frame: pd.DataFrame) -> plt.Figure:
         loc="left",
         fontsize=20.5,
         fontweight="bold",
-        pad=28,
+        pad=23,
     )
     axis.text(
         0.0,
